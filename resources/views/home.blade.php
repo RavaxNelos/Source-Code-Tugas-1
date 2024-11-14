e4335c22d36993d5e1f39643ae477eadc75e249a
RavaxNelos <nrava741@gmail.com>
2024-11-14 14:29:53 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,329 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ravanelo Earphone</title>
+    <!-- bootstrap css-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <header>
+        <div class="logo">RAVANELOS HEADSET</div>
+        <nav>
+            <a href="">Home</a>
+            <a href="">Info</a>
+            <a href="">Contact</a>
+        </nav>
+    </header>
+    <div class="carousel">
+        <div class="list">
+            <div class="item">
+                <img src="../img/earphone_1.png">
+                <div class="introduce">
+                    <div class="title">EARPHONE CUSTOM</div>
+                    <div class="topic">Earphone A</div>
+                    <div class="des">
+                        <!-- 20 lorem -->
+                        Earphone Hitam adalah perangkat audio ringkas yang dikenakan di telinga, menyuguhkan pengalaman
+                        mendengarkan pribadi dengan desain yang nyaman dan portabel.
+                    </div>
+                    <button class="seeMore">SEE MORE &#8599</button>
+                </div>
+                <div class="detail">
+                    <div class="title">EARPHONE HITAM</div>
+                    <div class="des">
+                        Earphone adalah perangkat audio pribadi yang dipakai di atau di sekitar telinga. Mereka hadir
+                        dalam berbagai gaya, termasuk in-ear dan over-ear, dengan fitur-fitur seperti konektivitas
+                        nirkabel, noise-canceling, dan kontrol.
+                    </div>
+                    <div class="specifications">
+                        <div>
+                            <p>Used Time</p>
+                            <p>6 hours</p>
+                        </div>
+                        <div>
+                            <p>Charging</p>
+                            <p>Type-C</p>
+                        </div>
+                        <div>
+                            <p>Compatible</p>
+                            <p>Android</p>
+                        </div>
+                        <div>
+                            <p>Bluetooth</p>
+                            <p>5.3</p>
+                        </div>
+                        <div>
+                            <p>Controlled</p>
+                            <p>Touch</p>
+                        </div>
+                    </div>
+                    <div class="checkout">
+                        <button>ADD TO CART</button>
+                        <button>CHECKOUT</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="../img/headset_2.png">
+                <div class="introduce">
+                    <div class="title">EARPHONE CUSTOM</div>
+                    <div class="topic">Earphone B</div>
+                    <div class="des">
+                        <!-- 20 lorem -->
+                        Earphone Hitam adalah perangkat audio ringkas yang dikenakan di telinga, menyuguhkan pengalaman
+                        mendengarkan pribadi dengan desain yang nyaman dan portabel.
+                    </div>
+                    <button class="seeMore">SEE MORE &#8599</button>
+                </div>
+                <div class="detail">
+                    <div class="title">EARPHONE HITAM</div>
+                    <div class="des">
+                        Earphone adalah perangkat audio pribadi yang dipakai di atau di sekitar telinga. Mereka hadir
+                        dalam berbagai gaya, termasuk in-ear dan over-ear, dengan fitur-fitur seperti konektivitas
+                        nirkabel, noise-canceling, dan kontrol.
+                    </div>
+                    <div class="specifications">
+                        <div>
+                            <p>Used Time</p>
+                            <p>6 hours</p>
+                        </div>
+                        <div>
+                            <p>Charging</p>
+                            <p>Type-C</p>
+                        </div>
+                        <div>
+                            <p>Compatible</p>
+                            <p>Android</p>
+                        </div>
+                        <div>
+                            <p>Bluetooth</p>
+                            <p>5.3</p>
+                        </div>
+                        <div>
+                            <p>Controlled</p>
+                            <p>Touch</p>
+                        </div>
+                    </div>
+                    <div class="checkout">
+                        <button>ADD TO CART</button>
+                        <button>CHECKOUT</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="../img/headset_3.png">
+                <div class="introduce">
+                    <div class="title">EARPHONE CUSTOM</div>
+                    <div class="topic">Earphone C</div>
+                    <div class="des">
+                        <!-- 20 lorem -->
+                        Earphone Biru Muda adalah perangkat audio ringkas yang dikenakan di telinga, menyuguhkan
+                        pengalaman
+                        mendengarkan pribadi dengan desain yang nyaman dan portabel.
+                    </div>
+                    <button class="seeMore">SEE MORE &#8599</button>
+                </div>
+                <div class="detail">
+                    <div class="title">EARPHONE BIRU MUDA</div>
+                    <div class="des">
+                        Earphone adalah perangkat audio pribadi yang dipakai di atau di sekitar telinga. Mereka hadir
+                        dalam berbagai gaya, termasuk in-ear dan over-ear, dengan fitur-fitur seperti konektivitas
+                        nirkabel, noise-canceling, dan kontrol.
+                    </div>
+                    <div class="specifications">
+                        <div>
+                            <p>Used Time</p>
+                            <p>6 hours</p>
+                        </div>
+                        <div>
+                            <p>Charging</p>
+                            <p>Type-C</p>
+                        </div>
+                        <div>
+                            <p>Compatible</p>
+                            <p>Android</p>
+                        </div>
+                        <div>
+                            <p>Bluetooth</p>
+                            <p>5.3</p>
+                        </div>
+                        <div>
+                            <p>Controlled</p>
+                            <p>Touch</p>
+                        </div>
+                    </div>
+                    <div class="checkout">
+                        <button>ADD TO CART</button>
+                        <button>CHECKOUT</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="../img/earphone_4.png">
+                <div class="introduce">
+                    <div class="title">EARPHONE CUSTOM</div>
+                    <div class="topic">Earphone D</div>
+                    <div class="des">
+                        <!-- 20 lorem -->
+                        Earphone Biru Tua adalah perangkat audio ringkas yang dikenakan di telinga, menyuguhkan
+                        pengalaman
+                        mendengarkan pribadi dengan desain yang nyaman dan portabel.
+                    </div>
+                    <button class="seeMore">SEE MORE &#8599</button>
+                </div>
+                <div class="detail">
+                    <div class="title">EARPHONE BIRU TUA</div>
+                    <div class="des">
+                        Earphone adalah perangkat audio pribadi yang dipakai di atau di sekitar telinga. Mereka hadir
+                        dalam berbagai gaya, termasuk in-ear dan over-ear, dengan fitur-fitur seperti konektivitas
+                        nirkabel, noise-canceling, dan kontrol.
+                    </div>
+                    <div class="specifications">
+                        <div>
+                            <p>Used Time</p>
+                            <p>6 hours</p>
+                        </div>
+                        <div>
+                            <p>Charging</p>
+                            <p>Type-C</p>
+                        </div>
+                        <div>
+                            <p>Compatible</p>
+                            <p>Android</p>
+                        </div>
+                        <div>
+                            <p>Bluetooth</p>
+                            <p>5.3</p>
+                        </div>
+                        <div>
+                            <p>Controlled</p>
+                            <p>Touch</p>
+                        </div>
+                    </div>
+                    <div class="checkout">
+                        <button>ADD TO CART</button>
+                        <button>CHECKOUT</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="../img/earphone_5.png">
+                <div class="introduce">
+                    <div class="title">EARPHONE CUSTOM</div>
+                    <div class="topic">Earphone E</div>
+                    <div class="des">
+                        <!-- 20 lorem -->
+                        Earphone Pink adalah perangkat audio ringkas yang dikenakan di telinga, menyuguhkan pengalaman
+                        mendengarkan pribadi dengan desain yang nyaman dan portabel.
+                    </div>
+                    <button class="seeMore">SEE MORE &#8599</button>
+                </div>
+                <div class="detail">
+                    <div class="title">EARPHONE PINKU</div>
+                    <div class="des">
+                        Earphone adalah perangkat audio pribadi yang dipakai di atau di sekitar telinga. Mereka hadir
+                        dalam berbagai gaya, termasuk in-ear dan over-ear, dengan fitur-fitur seperti konektivitas
+                        nirkabel, noise-canceling, dan kontrol.
+                    </div>
+                    <div class="specifications">
+                        <div>
+                            <p>Used Time</p>
+                            <p>6 hours</p>
+                        </div>
+                        <div>
+                            <p>Charging</p>
+                            <p>Type-C</p>
+                        </div>
+                        <div>
+                            <p>Compatible</p>
+                            <p>Android</p>
+                        </div>
+                        <div>
+                            <p>Bluetooth</p>
+                            <p>5.3</p>
+                        </div>
+                        <div>
+                            <p>Controlled</p>
+                            <p>Touch</p>
+                        </div>
+                    </div>
+                    <div class="checkout">
+                        <button>ADD TO CART</button>
+                        <button>CHECKOUT</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="../img/earphone_6.png">
+                <div class="introduce">
+                    <div class="title">EARPHONE CUSTOM</div>
+                    <div class="topic">Earphone F</div>
+                    <div class="des">
+                        <!-- 20 lorem -->
+                        Earphone Biru Muda adalah perangkat audio ringkas yang dikenakan di telinga, menyuguhkan
+                        pengalaman
+                        mendengarkan pribadi dengan desain yang nyaman dan portabel.
+                    </div>
+                    <button class="seeMore">SEE MORE &#8599</button>
+                </div>
+                <div class="detail">
+                    <div class="title">EARPHONE BIRU MUDA</div>
+                    <div class="des">
+                        Earphone adalah perangkat audio pribadi yang dipakai di atau di sekitar telinga. Mereka hadir
+                        dalam berbagai gaya, termasuk in-ear dan over-ear, dengan fitur-fitur seperti konektivitas
+                        nirkabel, noise-canceling, dan kontrol.
+                    </div>
+                    <div class="specifications">
+                        <div>
+                            <p>Used Time</p>
+                            <p>6 hours</p>
+                        </div>
+                        <div>
+                            <p>Charging</p>
+                            <p>Type-C</p>
+                        </div>
+                        <div>
+                            <p>Compatible</p>
+                            <p>Android</p>
+                        </div>
+                        <div>
+                            <p>Bluetooth</p>
+                            <p>5.3</p>
+                        </div>
+                        <div>
+                            <p>Controlled</p>
+                            <p>Touch</p>
+                        </div>
+                    </div>
+                    <div class="checkout">
+                        <button>ADD TO CART</button>
+                        <button>CHECKOUT</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="arrows">
+            <button id="prev"><i class="fa-solid fa-chevron-left fa-fw"></i></button>
+            <button id="back">Kembali &#8599</button>
+            <button id="next"><i class="fa-solid fa-chevron-right fa-fw"></i></button>
+        </div>
+    </div>
+
+
+
+    <script src="js/script.js"></script>
+</body>
+
+</html>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ravanelo Blog | Rava</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+<div class="container">
+    <header class="header">
+        <a href="#" class="logo">RAVA</a>
+
+        <nav class="navbar">
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/blog">Blog</a>
+            <a href="/contact"></a>
+        </nav>
+    </div>
+
+      <div class="container mt-7">
+        @yield('container')
+      </div>
+    
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
